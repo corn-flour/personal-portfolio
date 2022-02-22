fc18ccdc3f6a2bc87f77e5222305099579ef8bab
harry <26082352+bte234@users.noreply.github.com>
2022-02-21 22:01:31 -0800

    chore: initial project config
    Add header to site layout. Modify header nav with links to site pages.
    Create blank about, projects and contact page.
    Change meta title of site.
    Change prettier config. Set prettier tab width to 4 and semi to false.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,13 @@
-import * as React from 'react';
+import * as React from 'react'
+
+import Header from './Header'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  // Put Header or Footer Here
-  return <>{children}</>;
+    // Put Header or Footer Here
+    return (
+        <>
+            <Header />
+            {children}
+        </>
+    )
 }
