c068829b309e6d62919471008b1ffd5fae911002
harry <26082352+bte234@users.noreply.github.com>
2022-02-23 00:02:38 -0800

    feat: icon animation on dark mode toggle
    fix: remove duplicated menu in 404 page

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -3,16 +3,37 @@ import { NextPage } from 'next'
 import { useTheme } from 'next-themes'
 import React from 'react'
 import { BsMoonFill, BsSunFill } from 'react-icons/bs'
+import { animated, useSpring } from 'react-spring'
 
 type DarkModeToggleType = {
     className?: string
     label?: string | React.ReactNode
 }
 
-const DarkModeToggle: NextPage<DarkModeToggleType> = ({ className, label }) => {
-    const { theme, setTheme } = useTheme()
+const DarkModeIcon = () => {
+    const { theme } = useTheme()
+    const props = useSpring({
+        from: {
+            opacity: 0,
+            y: -24,
+        },
+        to: {
+            opacity: 1,
+            y: 0,
+        },
+    })
 
     const icon = theme === 'dark' ? <BsSunFill /> : <BsMoonFill />
+
+    return (
+        <animated.div style={props} key={theme}>
+            {icon}
+        </animated.div>
+    )
+}
+
+const DarkModeToggle: NextPage<DarkModeToggleType> = ({ className, label }) => {
+    const { theme, setTheme } = useTheme()
 
     return (
         <button
@@ -22,12 +43,12 @@ const DarkModeToggle: NextPage<DarkModeToggleType> = ({ className, label }) => {
                 theme === 'dark'
                     ? 'text-orange-400 hover:text-orange-200'
                     : 'text-blue-400 hover:text-blue-600',
-                'p-1 text-lg transition-all duration-300',
+                'rounded-full p-2 text-lg transition-all duration-300',
                 className
             )}
         >
             {label}
-            {icon}
+            <DarkModeIcon key={theme} />
         </button>
     )
 }
