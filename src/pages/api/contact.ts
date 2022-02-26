65dd7d08f3ae3b429eb5252cf03cd2900acc0f9a
harry <26082352+bte234@users.noreply.github.com>
2022-02-26 00:13:04 -0800

    feat(api): implement sending email in api body
    POST /api/contact now sends notif email
    POST /api/contact now returns error if missing fields

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,5 +1,47 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import nodemailer from 'nodemailer'
 
-export default function postContact(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json({ name: req.body.name })
+const postContact = (req: NextApiRequest, res: NextApiResponse) => {
+    //#region  //*=========== Validate fields ===========
+    const { name, email, message } = req.body
+    if (!name || !email || !message) {
+        return res.status(400).json({
+            name: 'err_missing_fields',
+            message: 'Required field(s) are missing.',
+        })
+    }
+    //#endregion  //*======== Validate fields ===========
+
+    const transporter = nodemailer.createTransport({
+        port: 465,
+        host: 'smtp.gmail.com',
+        auth: {
+            user: process.env.GMAIL_IDENTIFIER,
+            pass: process.env.GMAIL_PASSWORD,
+        },
+        secure: true,
+    })
+
+    const mailData = {
+        from: process.env.GMAIL_IDENTIFIER,
+        to: process.env.RECIPIENT_EMAIL,
+        subject: `Portfolio: ${req.body.name} sent a message`,
+        text: `${req.body.message} | Sent from: ${req.body.email}`,
+        html: `<div>${req.body.message}</div> <p>Sent from: ${req.body.email}</p>`,
+    }
+
+    transporter.sendMail(mailData, (err) => {
+        if (err) {
+            return res.status(500).json({
+                name: err.name,
+                error: err.message,
+            })
+        }
+    })
+
+    return res.status(200).json({
+        status: 'success',
+    })
 }
+
+export default postContact
