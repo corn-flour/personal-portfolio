6d68c743fd87c3a878536382e54a700b9879d3f2
harry <26082352+bte234@users.noreply.github.com>
2022-02-28 23:13:12 -0800

    feat(projects): filter project based on category

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,13 +1,42 @@
 import { InferGetStaticPropsType } from 'next'
-import React from 'react'
+import React, { useEffect } from 'react'
 
+import clsxm from '@/lib/clsxm'
 import { getAllContents } from '@/lib/mdx'
 
 import ContentCard from '@/components/card/ContentCard'
 
+const filters = [
+    {
+        name: 'all',
+        val: '',
+    },
+    {
+        name: 'web',
+        val: 'web',
+    },
+    {
+        name: 'other',
+        val: 'other',
+    },
+]
+
 const Projects = ({
     projects,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
+    const [filter, setFilter] = React.useState<string>('')
+    const [filteredProjects, setFilteredProjects] = React.useState(projects)
+
+    useEffect(() => {
+        setFilteredProjects(
+            projects.filter((project) => {
+                return filter === ''
+                    ? true
+                    : project.frontmatter.category === filter
+            })
+        )
+    }, [filter, projects])
+
     return (
         <div>
             <section className='layout my-16'>
@@ -15,9 +44,30 @@ const Projects = ({
                     my projects
                 </h1>
 
+                {/* #region  /**=========== Category Filter =========== */}
+                <div className='mt-16 mb-4 flex gap-4'>
+                    {filters.map((f, i) => (
+                        <button
+                            key={i}
+                            onClick={() => setFilter(f.val)}
+                            className={clsxm(
+                                'bg-light py-2 px-8',
+                                'text-xs uppercase',
+                                'transition-colors',
+                                'hover:bg-zinc-400 dark:hover:bg-zinc-500',
+                                f.val === filter &&
+                                    'bg-primary-400 hover:bg-primary-500 dark:bg-primary-700 dark:hover:bg-primary-600'
+                            )}
+                        >
+                            {f.name}
+                        </button>
+                    ))}
+                </div>
+                {/* #endregion  /**======== Category Filter =========== */}
+
                 {/* #region  /**=========== Project List =========== */}
-                <ul className='my-16 grid gap-4 md:grid-cols-2'>
-                    {projects.map((project, i) => (
+                <ul className='mb-16 grid gap-4 md:grid-cols-2'>
+                    {filteredProjects.map((project, i) => (
                         <li key={i}>
                             <ContentCard
                                 slug={project.slug}
