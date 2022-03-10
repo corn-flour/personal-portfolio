93bff7f5ffc38d1986fb6e99813a6b8dbc39130c
harry <26082352+bte234@users.noreply.github.com>
2022-03-10 02:22:22 -0800

    fix: fix og image logo break

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -12,7 +12,7 @@ export function openGraph({
     templateTitle,
     description,
     // !STARTERCONF Or, you can use my server with your own logo.
-    logo = 'https://harrynguyendev.com/images/logo.jpg',
+    logo = 'https://harrynguyendev.com/images/logo.png',
 }: OpenGraphType): string {
     const ogLogo = encodeURIComponent(logo)
     const ogSiteName = encodeURIComponent(siteName.trim())
